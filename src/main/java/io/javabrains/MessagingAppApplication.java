ef9d8e231bc9534b4d06e75e4670dca7db0852ee
vpaklatzis <vpaklatzis@gmail.com>
2022-05-10 20:10:18 +0300

    added DataStaxAstraProperties in order to connect with online db instance

--- src/main/java/io/javabrains/MessagingAppApplication.java
+++ src/main/java/io/javabrains/MessagingAppApplication.java
@@ -2,24 +2,22 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
-@RestController
+@EnableConfigurationProperties({DataStaxAstraProperties.class})
 public class MessagingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingAppApplication.class, args);
 	}
-
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
+	@Bean
+	public CqlSessionBuilderCustomizer cqlSessionBuilderCustomizer(DataStaxAstraProperties dataStaxAstraProperties) {
+		Path bundle = dataStaxAstraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
-	
-
 }
